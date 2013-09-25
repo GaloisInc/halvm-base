e85af9a2c8592785bf83c7a3dea79e5e9ba7b932
Adam Wick <awick@galois.com>
2013-09-25 13:53:59 -0700

    NoIO event manager, version 2.

--- GHC/Event/NoIO.hs
+++ GHC/Event/NoIO.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE RecursiveDo #-}
 {-# LANGUAGE Trustworthy #-}
 module GHC.Event.NoIO(
          ensureIOManagerIsRunning
@@ -10,29 +11,40 @@ module GHC.Event.NoIO(
  where
 
 import Data.Maybe(Maybe(..))
-import Foreign.StablePtr(StablePtr, newStablePtr)
+import Foreign.StablePtr(StablePtr, newStablePtr, deRefStablePtr, freeStablePtr)
 import GHC.Base
-import GHC.Conc.Sync(TVar, atomically, newTVar, writeTVar)
+import GHC.Conc.Sync(TVar, atomically, newTVar, writeTVar, forkIO)
 import GHC.MVar(MVar, newEmptyMVar, takeMVar, putMVar)
+import Foreign.C.String
+import Foreign.Ptr
 
 ensureIOManagerIsRunning :: IO ()
-ensureIOManagerIsRunning  = return ()
+ensureIOManagerIsRunning = forkIO runWaiters >> return ()
+ where
+  runWaiters = do
+    next <- waitForWaiter
+    action <- deRefStablePtr next
+    _ <- forkIO action
+    runWaiters
 
 ioManagerCapabilitiesChanged :: IO ()
 ioManagerCapabilitiesChanged  = return ()
 
 threadDelay :: Int -> IO ()
-threadDelay usecs = do
+threadDelay usecs = mdo
   wait <- newEmptyMVar
-  sp <- newStablePtr $ putMVar wait ()
+  sp <- newStablePtr $ putMVar wait () >> freeStablePtr sp
   registerWaiter usecs sp
   takeMVar wait
 
 registerDelay :: Int -> IO (TVar Bool)
-registerDelay usecs = do
+registerDelay usecs = mdo
   t <- atomically $ newTVar False
-  sp <- newStablePtr $ writeTVar t True
+  sp <- newStablePtr $ atomically (writeTVar t True) >> freeStablePtr sp
   return t
 
 foreign import ccall unsafe "registerWaiter"
   registerWaiter :: Int -> StablePtr (IO ()) -> IO ()
+
+foreign import ccall safe "waitForWaiter"
+  waitForWaiter :: IO (StablePtr (IO ()))
